c0ba6715ce54f362fe7a4f8777bfe62dcf985820
krjohnn <ks91ko@gmail.com>
2013-05-03 13:06:36 +0300

    md1 initial commit

---
+++ md1/md1.s
@@ -0,0 +1,24 @@
+	.text
+	.align	2
+	.global	apro
+	.type	apro, %function
+apro:
+    mov r1, #0
+    ble progress
+
+progress:
+    adds r1, r0
+    bvs err
+    sub r0, #1
+    cmp r0, #0
+    beq result
+    bge progress
+
+err:
+    mov r0, #-1
+    bx lr
+
+result:
+    mov r0, r1
+    bx lr
+    .size   apro, .-apro
---
+++ md1/md1_main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int apro(int a);
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2){
+        printf("Please input a value!\n");
+        return -1;
+    }else{
+        int a = atoi(argv[1]);
+        printf("apro(%d) = %d\n", a, apro(a));
+        return 0;
+    }
+}
